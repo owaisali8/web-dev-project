43edc4aed216b48368bc7087d559491e8e8a0add
Owais <owaisalisyed.8@gmail.com>
2023-04-20 20:19:47 +0500

    Vite React App Init
    - Vite for React Web App is initialized
    - React Router Initialized
    - Folder hierarchy updated

---
+++ frontend/web-app/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './routes/app/App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from './routes/error/error-page'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
